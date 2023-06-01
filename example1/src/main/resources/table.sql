89be78e3ae78b38b1c0d601afdff27f394386331
tong.wang <tong.wang3@bluefoucs.com>
2023-06-01 17:10:34 +0800

    修改建表SQL->去掉主键自增

--- example1/src/main/resources/table.sql
+++ example1/src/main/resources/table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `t_order_1` (
-	`order_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+	`order_id` BIGINT(20) NOT NULL COMMENT '主键',
 	`user_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '用户 id',
 	`total_price` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '订单价格',
 	`state` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '订单状态',
@@ -12,7 +12,7 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 CREATE TABLE `t_order_2` (
-	`order_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+	`order_id` BIGINT(20) NOT NULL COMMENT '主键',
 	`user_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '用户 id',
 	`total_price` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '订单价格',
 	`state` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '订单状态',
